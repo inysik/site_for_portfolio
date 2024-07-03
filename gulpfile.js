89050b748451920d1696cec0f0ac1c463103b55e
InessaRyzhikova <inessa1992@yandex.ru>
2024-07-03 19:00:03 +0300

    add other site

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
+
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const historyApiFallback = require('connect-history-api-fallback');
+
 
 gulp.task('sass', function() {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('src/css'))
-})
-
-gulp.task('watch', function() {
-  gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+    .pipe(browserSync.stream()); 
 });
 
-gulp.task('default', gulp.series('sass', 'watch'));
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: './', // Корневая директория вашего проекта
+            middleware: [historyApiFallback()] // Используем middleware для обработки маршрутов
+        },
+        startPath: 'index.html' // Начальная страница
+    });
+
+    // Слежение за изменениями файлов и перезагрузка браузера
+    gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch('src/css/**/*.css').on('change', browserSync.reload);
+    gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
+});
+
+// Задача по умолчанию
+gulp.task('default', gulp.series('sass', 'serve'));
+
